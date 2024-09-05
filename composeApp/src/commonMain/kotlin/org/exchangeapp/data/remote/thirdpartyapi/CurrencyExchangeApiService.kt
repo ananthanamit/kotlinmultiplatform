9205111946633958c63267182b4022e923e4bddc
ananthanamit <anan.thanamit@gmail.com>
2024-09-06 05:49:36 +0700

    commit

---
+++ composeApp/src/commonMain/kotlin/org/exchangeapp/data/remote/thirdpartyapi/CurrencyExchangeApiService.kt
@@ -0,0 +1,9 @@
+package org.exchangeapp.data.remote.thirdpartyapi
+
+class CurrencyExchangeApiService {
+    companion object{
+        const val EndPointApi = "https://api.currencyapi.com/v3/status"
+        const val ApiKeyCurrencyExchange = "cur_live_Cp7xrZq3FBh2HwzqgiFHapk8RwtLCphpilmb38E8"
+
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/exchangeapp/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.exchangeapp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
